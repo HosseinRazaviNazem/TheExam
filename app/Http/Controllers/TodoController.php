6a7378052f82ca82ba02b0e26a043d37dd3e356f
Sepehr <s.r.nazem@gmail.com>
2024-10-14 13:39:54 +0330

    add resource and collection - controller

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -7,4 +7,8 @@ use Illuminate\Http\Request;
 class TodoController extends Controller
 {
     //
+    public function index()
+    {
+        return new ProductCollection(Product::jsonPaginate());
+    }
 }
