1a27b26ec5bd740a343ae20938cd63bb2a236d65
Sepehr <s.r.nazem@gmail.com>
2024-10-15 20:24:39 +0330

    table

--- resources/views/livewire/task.blade.php
+++ resources/views/livewire/task.blade.php
@@ -130,57 +130,61 @@
     @endif
 
 
-
-
-
-    <table>
-            <thead>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Task</th>
+            <th>Description</th>
+            <th>Priority</th>
+            <th>Status</th>
+            <th>Deadline</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($todos as $todo)
             <tr>
-                <th>Task</th>
-                <th>Description</th>
-                <th>Priority</th>
-                <th>Status</th>
-                <th>Deadline</th>
-                <th>Actions</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($todos as $todo)
-                <tr>
-                    <td>
-                        <div class="d-flex px-2 py-1">
-                            <div>
-                                <img src="{{ asset('assets') }}/img/team-2.jpg"
-                                     class="avatar avatar-sm me-3 border-radius-lg"
-                                     alt="user1">
-                            </div>
-                            <div class="d-flex flex-column justify-content-center">
-                                <h6 class="mb-0 text-sm">{{ $todo->task_title }}</h6>
-                                <p class="text-xs text-secondary mb-0">{{ $todo->description }}</p>
-                            </div>
+                <td>
+                    <div class="d-flex px-2 py-1">
+                        <div class="d-flex flex-column justify-content-center">
+                            <h6 class="mb-0 text-sm">{{ $todo->task_title }}</h6>
                         </div>
-                    </td>
-                    <td>
-                        <p class="text-xs font-weight-bold mb-0">{{ $todo->priority }}</p>
-                        <p class="text-xs text-secondary mb-0">Priority</p>
-                    </td>
-                    <td class="align-middle text-center text-sm">
-                        <span class="badge badge-sm bg-gradient-success">{{ $todo->status }}</span>
-                    </td>
-                    <td class="align-middle text-center">
-{{--                        <span class="text-secondary text-xs font-weight-bold">{{ $todo->deadline ? $todo->deadline->format('Y-m-d') : 'No Deadline' }}</span>--}}
-                    </td>
-                    <td class="align-middle">
-                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                            Edit
-                        </a>
-                        <a href="javascript:;" wire:click="delete({{ $todo->id }})" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Delete task">
-                            Delete
-                        </a>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
+                    </div>
+                </td>
+
+                <td>
+                    <div class="d-flex px-2 py-1">
+                        <div class="d-flex flex-column justify-content-center">
+                            <p class="mb-0 text-sm">{{ Str::limit($todo->description, 15, '...') }}</p>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <p class="text-xs font-weight-bold mb-0">{{ $todo->priority }}</p>
+                    <p class="text-xs text-secondary mb-0">Priority</p>
+                </td>
+
+                <td class="align-middle text-center text-sm">
+                    <span class="badge badge-sm bg-gradient-success">{{ $todo->status }}</span>
+                </td>
+                <td class="align-middle text-center">
+                    <span class="text-secondary text-xs font-weight-bold">{{ $todo->deadline }}</span>
+                </td>
+                <td class="align-middle">
+                    <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip"
+                       data-original-title="Edit user">
+                        Edit
+                    </a>
+                    <a href="javascript:;" wire:click="delete({{ $todo->id }})"
+                       class="text-secondary font-weight-bold text-xs" data-toggle="tooltip"
+                       data-original-title="Delete task">
+                        Delete
+                    </a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+
 
 </div>
